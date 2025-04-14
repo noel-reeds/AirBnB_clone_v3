40135de3b3b84f9eacb86fead6d458d52315c231
altoids <noelreeds01@gmail.com>
2025-04-15 00:36:56 +0300

    fixes an exception

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,8 +55,8 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except Exception as err:
-            raise NotImplementedError
+        except:
+            pass
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
