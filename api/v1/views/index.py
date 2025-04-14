47499319486662d1416f87bf9c5261e8342485f9
altoids <noelreeds01@gmail.com>
2025-04-15 01:44:06 +0300

    endpoint that retrieves the number of each objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,27 @@
 """basic routes for the Flask App"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage, classes
+
 
 @app_views.route('/status')
 def status_api():
     """Tests the status of API"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    """retrieves the number of each objects"""
+    # query storage engine for each object
+    objs = {}
+    for key, cls in classes.items():
+        # change key into lower case for returned JSON
+        key = key.lower()
+        # convert keys into plurals
+        # yes, i was bored  
+        if key.endswith('y'):
+            key = key.replace('y', 'ies')
+        else:
+            key = key + 's'
+        objs[key] = storage.count(cls)
+    return jsonify(objs)
