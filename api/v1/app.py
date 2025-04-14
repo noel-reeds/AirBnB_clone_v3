7c704554519dc305d37710d77baec6adfad93532
altoids <noelreeds01@gmail.com>
2025-04-15 00:20:49 +0300

    sets up a flask app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
-from flask import Flask, jsonify
+"""
+initializes a basic Flask app
+"""
+import os
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
 
+# instantiates Flask
 app = Flask(__name__)
 
-@app.route('/api/v1/status')
-def api_status():
-    return jsonify({ "status" : "OK" })
+# register blueprints
+app.register_blueprint(app_views)
 
+@app.teardown_appcontext(Exception)
+def teardown_db(exception):
+    """closes storage engine"""
+    storage.close()
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST')
+    port = os.getenv('HBNB_API_PORT')
+    if host and port:
+        app.run(host=host, port=port, threaded=True, debug=True)
+    else:
+        app.run(host='0.0.0.0', port='5000', threaded=True, debug=True)
