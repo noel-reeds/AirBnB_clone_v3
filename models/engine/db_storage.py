e7e319515c19ba94e94fcac095c01dac3ff39e46
altoids <noelreeds01@gmail.com>
2025-04-14 13:35:02 +0300

    fixes pep8 validation

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,7 +74,7 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-    
+
     def get(self, cls, id):
         """retrieves an object of the specified class and id"""
         # check if cls exists in models
