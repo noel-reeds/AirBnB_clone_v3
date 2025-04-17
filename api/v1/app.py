c0c502f7a4202081c0dd78d0441a71293f033977
altoids <noelreeds01@gmail.com>
2025-04-17 03:40:43 +0300

    registers cities views blueprint

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ initializes a basic Flask app
 import os
 from flask import Flask, jsonify
 from models import storage
-from api.v1.views import app_views, states_views
+from api.v1.views import app_views, states_views, cities_views
 
 # instantiates Flask
 app = Flask(__name__)
@@ -14,7 +14,7 @@ app.url_map.strict_slashes = False
 # register blueprints
 app.register_blueprint(app_views)
 app.register_blueprint(states_views)
-
+app.register_blueprint(cities_views)
 
 @app.teardown_appcontext(Exception)
 def teardown_db(exception):
